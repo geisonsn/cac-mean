63fa50b6a28d3c0643384138e8e7125754262b9f
geisonsn <geison.sn@gmail.com>
2016-09-28 15:56:33 -0400

    Instalação e configuração do MongoDB

--- projeto/contatooh/server.js
+++ projeto/contatooh/server.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var app = require('./config/express')();
+require('./config/database.js')('mongodb://localhost/contatooh');
 
 http.createServer(app).listen(app.get('port'), () => { 
     console.log('Express server escutando na port ' + app.get('port'));
