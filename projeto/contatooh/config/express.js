be2c5d31948ed3f01837dc1159f6c3f4a27a3b14
geisonsn <geison.sn@gmail.com>
2016-10-04 09:37:55 -0400

    Configuração de segurança com o Helmet

--- projeto/contatooh/config/express.js
+++ projeto/contatooh/config/express.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
+var helmet = require('helmet');
 
 module.exports = () => {
     const app = express();
@@ -27,6 +28,13 @@ module.exports = () => {
     }));
     app.use(passport.initialize());
     app.use(passport.session());
+
+    //app.use(helmet()); //habilita todos os middlewares de segurança do Helmet
+    //app.use(helmet.hidePoweredBy({setTo: 'PHP 5.5.14'})); //oculta o valor de X-Powered-By e o substitui
+    app.use(helmet.frameguard());//restringe a inclusão de páginas em frames
+    app.use(helmet.xssFilter());//adiciona proteção contra XSS na aplicação
+    app.use(helmet.noSniff());//adiciona proteção para bloquear o load de arquivos com Mime type diferente
+    app.disable('x-powered-by'); //desabitando X-Powered-By para não exibir versão do servidor
 
     consign({cwd: 'app'})
         .include('models')
