8af79d966004f01e0241d1893405c22ebade5ecc
geisonsn <geison.sn@gmail.com>
2016-10-10 14:50:51 -0400

    Refatoração para uso do padrão PageObject

--- projeto/contatooh/test/e2e/contatosPageSpec.js
+++ projeto/contatooh/test/e2e/contatosPageSpec.js
@@ -1,23 +1,24 @@
+var ContatosPage = require('./pages/contatosPage');
+
 describe('Página principal', function() {
+    var pagina = new ContatosPage();
+
     beforeEach(function() {
-        browser.get('http://localhost:3000/#/contatos');
+        pagina.visitar();
     });
 
     it('Deve estar logado', function() {
-        element(by.id('usuario-logado')).getText()
-            .then(function(texto) {
-                expect(texto.trim().length).toBeGreaterThan(0);
-            });
+        pagina.obterUsuarioLogado().then(function(texto) {
+            expect(texto.trim().length).toBeGreaterThan(0);
+        });
     });
 
     it('Deve remover um contato da lista', function(){
-        var totalAntes = element.all(by.repeater('contato in contatos')).count();
+        var totalAntes = pagina.obterTotalDeItensDaLista(); 
 
-        element(by.repeater('contato in contatos').row(0))
-            .element(by.css('.btn'))
-            .click();
+        pagina.removerPrimeiroItemDaLista();
 
-        var totalDepois = element.all(by.repeater('contato in contatos')).count();
+        var totalDepois = pagina.obterTotalDeItensDaLista();
         expect(totalDepois).toBeLessThan(totalAntes);
     });
 });
