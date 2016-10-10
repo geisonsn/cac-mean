e36752f5cd305f1717aadc55e19a89bc1a1ded16
Geison Santos <geison.sn@gmail.com>
2016-10-09 21:09:08 -0400

    Definição de teste automatizado na autenticação do usuário com Selenium e Protractor

---
+++ projeto/contatooh/config/protractor.js
@@ -0,0 +1,14 @@
+exports.config = {
+    specs: ['../test/e2e/**/*.js'],
+    onPrepare: function() {
+        browser.driver.get('http://localhost:3000')
+            .then(function() {
+                browser.driver.findElement(by.id('entrar')).click();
+                browser.driver.findElement(by.id('login_field'))
+                    .sendKeys('gitHubUser');
+                browser.driver.findElement(by.id('password'))
+                    .sendKeys('gitHubPassword');
+                browser.driver.findElement(by.name('commit')).click();
+        });
+    }
+};
