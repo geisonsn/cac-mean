e6a2525f770dcbc5e4a35c698dc7f5fac1e32395
geisonsn <geison.sn@gmail.com>
2016-10-10 10:47:05 -0400

    Testando exclusão de contato

--- projeto/contatooh/test/e2e/contatosPageSpec.js
+++ projeto/contatooh/test/e2e/contatosPageSpec.js
@@ -9,4 +9,15 @@ describe('Página principal', function() {
                 expect(texto.trim().length).toBeGreaterThan(0);
             });
     });
+
+    it('Deve remover um contato da lista', function(){
+        var totalAntes = element.all(by.repeater('contato in contatos')).count();
+
+        element(by.repeater('contato in contatos').row(0))
+            .element(by.css('.btn'))
+            .click();
+
+        var totalDepois = element.all(by.repeater('contato in contatos')).count();
+        expect(totalDepois).toBeLessThan(totalAntes);
+    });
 });
